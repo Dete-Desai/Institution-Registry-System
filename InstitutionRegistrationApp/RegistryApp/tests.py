066d34a50e6f5f51ac3b0a4cb99a451eee0534c0
Dete-Desai <detedesai@gmail.com>
2021-03-09 17:03:41 +0300

    Improvements

--- InstitutionRegistrationApp/RegistryApp/tests.py
+++ InstitutionRegistrationApp/RegistryApp/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import patientregistrationformdb
+from . import views
+
+
 
 # Create your tests here.
---
+++ InstitutionRegistrationApp/Tests.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+
+# Create your tests here.
+# See tests.py file inside the RegistryApp folder
